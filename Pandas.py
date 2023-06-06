d5356a4eb2c9dc0bc364aa108b9262ba3b296f6c
Mahmud Hasan Mubin <59206457+MH-Mubin@users.noreply.github.com>
2023-06-06 23:34:56 +0600

    Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -1,4 +1,4 @@
-1. How many people of each race are represented in this dataset? This should be a Pandas series with race names as the index labels. (race column)
+1.How many people of each race are represented in this dataset? This should be a Pandas series with race names as the index labels. (race column)
 
 import pandas as pd
 #race count
@@ -6,7 +6,7 @@ df=pd.read_csv(r'/content/adult.data.csv')
 race_counts= df['race'].value_counts()
 print(race_counts)
 
-2. What is the average age of men?
+2.What is the average age of men?
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
@@ -14,7 +14,7 @@ male_df = df[df['sex'] == 'Male']
 average_age_men = male_df['age'].mean()
 print(average_age_men)
 
-3. What is the percentage of people who have a Bachelor's degree?
+3.What is the percentage of people who have a Bachelor's degree?
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
@@ -22,7 +22,7 @@ education_counts = df['education'].value_counts()
 percentage_bachelors = (education_counts['Bachelors'] / df.shape[0]) * 100
 print(percentage_bachelors)
 
-4. What percentage of people with advanced education (Bachelors, Masters, or Doctorate) make more than 50K?
+4.What percentage of people with advanced education (Bachelors, Masters, or Doctorate) make more than 50K?
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
@@ -31,7 +31,7 @@ filtered_df = df[df['education'].isin(advanced_education) & (df['salary'] == '>5
 percentage_high_income = (filtered_df.shape[0] / df[df['education'].isin(advanced_education)].shape[0]) * 100
 print(percentage_high_income)
 
-5. What percentage of people without advanced education make more than 50K?
+5.What percentage of people without advanced education make more than 50K?
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
@@ -40,14 +40,14 @@ filtered_df = df[~df['education'].isin(advanced_education) & (df['salary'] == '>
 percentage_high_income = (filtered_df.shape[0] / df[~df['education'].isin(advanced_education)].shape[0]) * 100
 print(percentage_high_income)
 
-6. What is the minimum number of hours a person works per week?
+6.What is the minimum number of hours a person works per week?
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
 min_hours_per_week = df['hours-per-week'].min()
 print("Minimum number of hours worked per week:", min_hours_per_week)
 
-7. What percentage of the people who work the minimum number of hours per week have a salary of more than 50K?
+7.What percentage of the people who work the minimum number of hours per week have a salary of more than 50K?
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
@@ -56,7 +56,7 @@ filtered_df = df[(df['hours-per-week'] == min_hours_per_week) & (df['salary'] ==
 percentage_of_high_income = (filtered_df.shape[0] / df[df['hours-per-week'] == min_hours_per_week].shape[0]) * 100
 print(percentage_of_high_income)
 
-8. What country has the highest percentage of people that earn >50K and what is that percentage?
+8.What country has the highest percentage of people that earn >50K and what is that percentage?
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
@@ -67,7 +67,7 @@ highest_percentage = percentages.max()
 print("Country with the highest percentage:", highest_percentage_country)
 print("Percentage:", highest_percentage)
 
-9. Identify the most popular occupation for those who earn >50K in India.
+9.Identify the most popular occupation for those who earn >50K in India.
 
 import pandas as pd
 df=pd.read_csv(r'/content/adult.data.csv') 
